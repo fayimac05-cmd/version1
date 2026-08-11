a28f7339249f098cc59ccfe327ff252acc62fe83
seres3 <seres3787@gmail.com>
2026-08-11 16:03:11 +0000

    liaison avec la base de donnéee 2/s

--- backend/src/config/supabase.js
+++ backend/src/config/supabase.js
@@ -3,7 +3,7 @@ const ws = require('ws');
 require('dotenv').config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_KEY || process.env.SUPABASE_ANON_KEY;
+const supabaseKey = process.env.SUPABASE_SERVICE_KEY || process.env.SUPABASE_SECRET_KEY || process.env.SUPABASE_ANON_KEY || process.env.SUPABASE_PUBLISHABLE_KEY;
 
 let supabase = null;
 
